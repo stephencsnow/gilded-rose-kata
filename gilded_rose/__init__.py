be0e86900de7cb1e659957f38a30e0ea98da651b
Stephen Snow <stephen.c.snow@gmail.com>
2022-09-09 18:50:45 -0400

    Refactor to introduce ItemWrapper
    No other changes to functionality. This approach was used because this
    seems like a problem where object-oriented is good. Different items
    respond different to similar calls (to update their quality).
    
    Next step will involve splitting out the quality and sell in steps

--- gilded_rose/__init__.py
+++ gilded_rose/__init__.py
@@ -1,39 +1,47 @@
-# -*- coding: utf-8 -*-
+class ItemWrapper:
+    def __init__(self, item: "Item"):
+        self.item = item
+
+    def update_quality(self):
+        if (
+            self.item.name != "Aged Brie"
+            and self.item.name != "Backstage passes to a TAFKAL80ETC concert"
+        ):
+            if self.item.quality > 0:
+                if self.item.name != "Sulfuras, Hand of Ragnaros":
+                    self.item.quality = self.item.quality - 1
+        else:
+            if self.item.quality < 50:
+                self.item.quality = self.item.quality + 1
+                if self.item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if self.item.sell_in < 11:
+                        if self.item.quality < 50:
+                            self.item.quality = self.item.quality + 1
+                    if self.item.sell_in < 6:
+                        if self.item.quality < 50:
+                            self.item.quality = self.item.quality + 1
+        if self.item.name != "Sulfuras, Hand of Ragnaros":
+            self.item.sell_in = self.item.sell_in - 1
+        if self.item.sell_in < 0:
+            if self.item.name != "Aged Brie":
+                if self.item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if self.item.quality > 0:
+                        if self.item.name != "Sulfuras, Hand of Ragnaros":
+                            self.item.quality = self.item.quality - 1
+                else:
+                    self.item.quality = self.item.quality - self.item.quality
+            else:
+                if self.item.quality < 50:
+                    self.item.quality = self.item.quality + 1
+
 
 class GildedRose(object):
-
     def __init__(self, items):
-        self.items = items
+        self.items = [ItemWrapper(item) for item in items]
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update_quality()
 
 
 class Item:
